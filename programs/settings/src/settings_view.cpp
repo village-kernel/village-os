d416687b4d5bdc48ff286926b4adae5309ddedc5
aicodeone <277579066@qq.com>
2025-01-18 18:12:25 +0800

    village-os：废弃的os

---
+++ programs/settings/src/settings_view.cpp
@@ -0,0 +1,15 @@
+//###########################################################################
+// settings_view.cpp
+// Definitions of the functions that manage settings view
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#include "settings_view.h"
+
+
+/// @brief Init Component
+/// @param parent 
+void SettingsView::InitComponent(VgWedget* parent)
+{
+
+}
